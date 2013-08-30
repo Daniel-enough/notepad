12f22453a995d15ffcfaa936a79c54dff2ab793f
Daniel Rieckers <daniel.rieckers@enough.de>
2013-08-30 13:27:27 +0200

    NOTEPAD-5 Store a note - store notes by pressing back button of the phone - check if EditText fields filled out correct or empty - created onBackKeyListener interface in Main Activity

--- src/de/enough/notepad/MainActivity.java
+++ src/de/enough/notepad/MainActivity.java
@@ -1,23 +1,75 @@
 package de.enough.notepad;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentManager.BackStackEntry;
+import android.support.v4.app.FragmentTransaction;
+import android.view.KeyEvent;
 import android.view.Menu;
 
 public class MainActivity extends FragmentActivity {
+
+	public interface OnBackKeyListener {
+		boolean onBackKeyDown();
+	}
 	
+	private static final String TAG_FRAGMENT_NOTE = NoteFragment.class.getName();
+	
+	private NoteFragment mNoteFragment;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		if (savedInstanceState != null) {
+			return;
+		}
+		
+		mNoteFragment = new NoteFragment();
+		
+		FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+		fragmentTransaction.add(R.id.layout_holder, mNoteFragment, TAG_FRAGMENT_NOTE);
+		fragmentTransaction.addToBackStack(TAG_FRAGMENT_NOTE);
+		fragmentTransaction.commit();  
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		Fragment currentFragment = getCurrentFragment();
+		boolean isOnBackKeyDownListener = currentFragment instanceof OnBackKeyListener;
+		if (keyCode != KeyEvent.KEYCODE_BACK || !isOnBackKeyDownListener) {
+			return super.onKeyDown(keyCode, event);
+		}
+
+		OnBackKeyListener onBackKeyDownListener = (OnBackKeyListener) currentFragment;
+		boolean handled = onBackKeyDownListener.onBackKeyDown();
+		if (!handled) {
+			return true;
+		} else {
+			return super.onKeyDown(keyCode, event);
+		}
+	}
+
+	public Fragment getCurrentFragment() {
+		FragmentManager supportFragmentManager = getSupportFragmentManager();
+		if (supportFragmentManager.getBackStackEntryCount() == 0) {
+			return null;
+		}
+		int currentFragmentIndex = supportFragmentManager
+				.getBackStackEntryCount() - 1;
+		BackStackEntry backStackEntry = supportFragmentManager
+				.getBackStackEntryAt(currentFragmentIndex);
+		String tag = backStackEntry.getName();
+		return supportFragmentManager.findFragmentByTag(tag);
+	}
+
 }
--- src/de/enough/notepad/NoteFragment.java
+++ src/de/enough/notepad/NoteFragment.java
@@ -1,17 +1,39 @@
 package de.enough.notepad;
 
+import de.enough.notepad.MainActivity.OnBackKeyListener;
+import de.enough.notepad.provider.NotesProvider;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class NoteFragment extends Fragment {
+public class NoteFragment extends Fragment implements OnBackKeyListener {
+
+	
+	private static final String TAG = NoteFragment.class.getName();
+
+	private Context mContext;
+	private ContentResolver mContentResolver;
 	
 	private EditText mInputTitle;
 	private EditText mInputDescription;
 
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	        Bundle savedInstanceState) {
@@ -24,7 +46,75 @@ public class NoteFragment extends Fragment {
 	public void setupInput(View contentView) {
 		mInputTitle = (EditText) contentView.findViewById(R.id.input_title);
 		mInputDescription = (EditText) contentView.findViewById(R.id.input_description);
+	}	
+
+	@Override
+	public void onAttach(Activity activity) {
+		mContext = getActivity().getBaseContext();
+		mContentResolver = mContext.getContentResolver();		
+		super.onAttach(activity);
+	} 
+	
+	@Override
+	public void onDestroy() {	
+		printNotes();
+		super.onDestroy();
+	}
+	
+	public boolean storeNote(String title, String description, long timestamp) {
+		
+		String selection = NotesProvider.KEY_TITLE + "=?";
+		String[] selectionArgs = {title};
+		
+		Cursor cursor = mContentResolver.query(NotesProvider.CONTENT_URI, null, 
+				selection, selectionArgs, null);
+		
+		if (cursor.getCount() != 0) {
+			String message = "Note: " + title + " allready exists"; 
+			Log.i(TAG, message);	
+			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		
+		ContentValues values = new ContentValues();	
+		values.put(NotesProvider.KEY_TITLE, title);
+		values.put(NotesProvider.KEY_DESCRIPTION, description);
+		values.put(NotesProvider.KEY_TIMESTAMP, timestamp);
+		mContentResolver.insert(NotesProvider.CONTENT_URI, values); 
+		return true;
+	}
+	
+	public void printNotes() {
+		
+		Cursor cursor = mContentResolver.query(NotesProvider.CONTENT_URI, null, null,
+				null, null);
+		
+		cursor.moveToFirst();
+		
+		for (int i = 0; i < cursor.getCount(); i++) {
+			Log.i(TAG , "Title: " + cursor.getString(cursor.getColumnIndex(NotesProvider.KEY_TITLE)));
+			cursor.moveToNext();
+		}
 	}
 
-	
+	@Override
+	public boolean onBackKeyDown() {
+		String title = mInputTitle.getText().toString();
+		String description = mInputDescription.getText().toString();
+		Long timestamp = System.currentTimeMillis();
+		
+		if (TextUtils.isEmpty(title.trim()) && TextUtils.isEmpty(description.trim())) {
+			return true;
+		} 
+		
+		if (TextUtils.isEmpty(title.trim()) || TextUtils.isEmpty(description.trim())) {
+			String message = "Fill in or clear both input fields"; 
+			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		
+		boolean noteStored = storeNote(title, description, timestamp);
+		return noteStored;
+	}
+
 }
