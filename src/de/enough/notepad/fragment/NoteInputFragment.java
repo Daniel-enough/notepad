0d9fa7aec94b24e44473435b64ebb58284668e6a
Daniel Rieckers <daniel.rieckers@enough.de>
2013-09-10 10:40:05 +0200

    NOTEPAD-8 UPDATE A NOTE - made list of notes clickable - opens input screen and show the NoteFragment with the respective title   and description of the entry on a click - detects if note is edited - updates entry in the database - checks if edited title allready exists - checks if title or description is empty after editing - created showCreate() and showUpdateNote() method in main activity - fixed variable name in NoteInputFragment

--- src/de/enough/notepad/fragment/NoteInputFragment.java
+++ src/de/enough/notepad/fragment/NoteInputFragment.java
@@ -2,13 +2,16 @@ package de.enough.notepad.fragment;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
 import android.text.TextUtils;
-import android.util.Log;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,14 +24,21 @@ import de.enough.notepad.provider.NotesProvider;
 public class NoteInputFragment extends Fragment implements OnBackKeyListener {
 
 	
-	private static final String TAG = NoteInputFragment.class.getName();
+	public static final String EXTRA_NOTE_ID = "id";
 
 	private Context mContext;
 	private ContentResolver mContentResolver;
 	
 	private EditText mInputTitle;
 	private EditText mInputDescription;
+	
+	private String mTitleInDatabase;
+	private String mDescriptionInDatabase;
 
+	private boolean mIsUpdate;
+	private boolean mTitleChanged;
+	private boolean mDescriptionChanged;
+	
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -41,13 +51,27 @@ public class NoteInputFragment extends Fragment implements OnBackKeyListener {
 
 		View contentView = inflater.inflate(R.layout.fragment_note_input, container, false);
 		setupInput(contentView);
+		
 		return contentView;
 	}
 	
-	public void setupInput(View contentView) {
-		mInputTitle = (EditText) contentView.findViewById(R.id.input_title);
-		mInputDescription = (EditText) contentView.findViewById(R.id.input_description);
-	}	
+	@Override
+	public void onStart() {
+		
+		Bundle data = getArguments();
+		
+		mIsUpdate = data != null;
+		
+		if (mIsUpdate) {
+			setStringsFromDatabase(data);
+			setEditText(data);
+		}
+		
+		mTitleChanged = false;
+		mDescriptionChanged = false;
+		
+		super.onStart();
+	}
 
 	@Override
 	public void onAttach(Activity activity) {
@@ -57,12 +81,48 @@ public class NoteInputFragment extends Fragment implements OnBackKeyListener {
 	} 
 	
 	@Override
-	public void onDestroy() {	
-		printNotes();
-		super.onDestroy();
+	public boolean onBackKeyDown() {
+		
+		if (mIsUpdate) {
+			Bundle data = getArguments();
+			boolean getBack = updateNote(data);	
+			return getBack;
+		}
+		
+		boolean noteStored = storeNote();
+		return noteStored;
 	}
 	
-	public boolean storeNote(String title, String description, long timestamp) {
+	public void setEditText(Bundle data) {
+		
+		Long id = data.getLong(EXTRA_NOTE_ID);
+
+		Uri selectedNoteUri = ContentUris.withAppendedId(NotesProvider.CONTENT_URI, id);
+		
+		Cursor cursor = mContentResolver.query(selectedNoteUri, null, null,
+				null, null);
+		
+		cursor.moveToFirst();
+		
+		String title = cursor.getString(cursor.getColumnIndex(NotesProvider.KEY_TITLE));
+		mInputTitle.setText(title);
+	
+		String description = cursor.getString(cursor.getColumnIndex(NotesProvider.KEY_DESCRIPTION));
+		mInputDescription.setText(description);		
+	}
+	
+	public void setupInput(View contentView) {
+		
+		TextWatcher watcher = createTextWatcher();
+		
+		mInputTitle = (EditText) contentView.findViewById(R.id.input_title);
+		mInputTitle.addTextChangedListener(watcher);
+		
+		mInputDescription = (EditText) contentView.findViewById(R.id.input_description);
+		mInputDescription.addTextChangedListener(watcher);
+	}
+	
+	public boolean checkIfNoteExists(String title) {
 		
 		String selection = NotesProvider.KEY_TITLE + "=?";
 		String[] selectionArgs = {title};
@@ -71,8 +131,36 @@ public class NoteInputFragment extends Fragment implements OnBackKeyListener {
 				selection, selectionArgs, null);
 		
 		if (cursor.getCount() != 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean storeNote() {
+		
+		String title = mInputTitle.getText().toString();
+		title = title.trim();
+		
+		String description = mInputDescription.getText().toString();
+		description = description.trim();
+		
+		Long timestamp = System.currentTimeMillis();
+		
+		if (TextUtils.isEmpty(title) && TextUtils.isEmpty(description)) {
+			String message = "Input canceled"; 
+			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+			return true;
+		} 
+		
+		if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
+			String message = "Fill in or clear both input fields"; 
+			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+			return false;
+		}
+
+		boolean noteExists = checkIfNoteExists(title);
+		if (noteExists) {
 			String message = "Note: " + title + " allready exists"; 
-			Log.i(TAG, message);	
 			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
 			return false;
 		}
@@ -81,47 +169,117 @@ public class NoteInputFragment extends Fragment implements OnBackKeyListener {
 		values.put(NotesProvider.KEY_TITLE, title);
 		values.put(NotesProvider.KEY_DESCRIPTION, description);
 		values.put(NotesProvider.KEY_TIMESTAMP, timestamp);
-		mContentResolver.insert(NotesProvider.CONTENT_URI, values); 
+		mContentResolver.insert(NotesProvider.CONTENT_URI, values);
 		
-		mInputTitle.getText().clear();
-		mInputDescription.getText().clear();
+		String message = "Note: " + title + " added"; 
+		Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
 		
 		return true;
 	}
 	
-	public void printNotes() {
+	public boolean updateNote(Bundle data) {
 		
-		Cursor cursor = mContentResolver.query(NotesProvider.CONTENT_URI, null, null,
-				null, null);
-		
-		cursor.moveToFirst();
-		
-		for (int i = 0; i < cursor.getCount(); i++) {
-			Log.i(TAG , "Title: " + cursor.getString(cursor.getColumnIndex(NotesProvider.KEY_TITLE)));
-			cursor.moveToNext();
+		if (!mTitleChanged && !mDescriptionChanged) {
+			String message = "Edit canceled"; 
+			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+			return true;
 		}
-	}
-
-	@Override
-	public boolean onBackKeyDown() {
+		
+		Long id = data.getLong(EXTRA_NOTE_ID);
+		Long timestamp = System.currentTimeMillis();	
+		
 		String title = mInputTitle.getText().toString();
 		title = title.trim();
+		
 		String description = mInputDescription.getText().toString();
 		description = description.trim();
-		Long timestamp = System.currentTimeMillis();
 		
-		if (TextUtils.isEmpty(title.trim()) && TextUtils.isEmpty(description.trim())) {
+		if (TextUtils.isEmpty(title) && TextUtils.isEmpty(description)) {
+			String message = "Edit canceled"; 
+			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
 			return true;
-		} 
+		}
 		
-		if (TextUtils.isEmpty(title.trim()) || TextUtils.isEmpty(description.trim())) {
+		if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
 			String message = "Fill in or clear both input fields"; 
 			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
 			return false;
 		}
 		
-		boolean noteStored = storeNote(title, description, timestamp);
-		return noteStored;
-	}
+		if (checkIfNoteExists(title) && !mTitleInDatabase.equalsIgnoreCase(title)) {
+			String message = "Note: " + title + " allready exists"; 
+			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+			return false;
+		}
 
+		Uri selectedNoteUri = ContentUris.withAppendedId(NotesProvider.CONTENT_URI, id);
+		
+		ContentValues values = new ContentValues();	
+		
+		if (mTitleChanged) {
+			values.put(NotesProvider.KEY_TITLE, title);
+		}
+		if (mDescriptionChanged) {
+			values.put(NotesProvider.KEY_DESCRIPTION, description);
+		}
+		
+		values.put(NotesProvider.KEY_TIMESTAMP, timestamp);
+		int count = mContentResolver.update(selectedNoteUri, values, null, null);
+		
+		if (count > 0) {
+			String message = "Note has been updated"; 
+			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		return false;
+	}
+	
+	public TextWatcher createTextWatcher() {
+		
+		return new TextWatcher() {
+			@Override
+			public void afterTextChanged(Editable s) {
+				checkTextChange();
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+			}
+		};
+	}
+	
+	public void setStringsFromDatabase(Bundle data) {
+		
+		Long id = data.getLong(EXTRA_NOTE_ID);
+		
+		Uri selectedNoteUri = ContentUris.withAppendedId(NotesProvider.CONTENT_URI, id);
+		
+		Cursor cursor = mContentResolver.query(selectedNoteUri, null, null,
+				null, null);
+		
+		cursor.moveToFirst();
+		
+		mTitleInDatabase = cursor.getString(cursor.getColumnIndex(NotesProvider.KEY_TITLE));
+		mDescriptionInDatabase = cursor.getString(cursor.getColumnIndex(NotesProvider.KEY_DESCRIPTION));	
+	}
+	
+	public void checkTextChange() {
+		
+		String titleInEditText = mInputTitle.getText().toString();
+		titleInEditText = titleInEditText.trim();
+		String descriptionInEditText = mInputDescription.getText().toString();
+		descriptionInEditText = descriptionInEditText.trim();
+
+		if (mIsUpdate) {
+			mTitleChanged = !mTitleInDatabase.equalsIgnoreCase(titleInEditText);
+			mDescriptionChanged = !mDescriptionInDatabase.equalsIgnoreCase(descriptionInEditText);
+		}
+	}
+	
 }
